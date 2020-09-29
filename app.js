16c189b3e1ddaf0eec2506c8896f9e82752d0b1a
Mai Yamawaki <joooooy031006@icloud.com>
2020-09-29 18:16:39 -0500

    first deploy

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -52,6 +52,6 @@ app.use('/', auth);
 
 
 // Uncomment this line for production
-// app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/*', (req, res) => res.sendFile(__dirname + '/public/build/index.html'));
 
 module.exports = app;
---
+++ public/build/precache-manifest.8e348274def42ed611d452840a1899e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6492febc66837751f1c281feac34c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d82dccaecb01b160e29",
+    "url": "/static/css/main.cc4cb6ef.chunk.css"
+  },
+  {
+    "revision": "05f49cf4c982f2fa5021",
+    "url": "/static/js/2.83830c24.chunk.js"
+  },
+  {
+    "revision": "6d82dccaecb01b160e29",
+    "url": "/static/js/main.696cffee.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
